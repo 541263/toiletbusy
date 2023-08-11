37aaa5b6c8527bd937eca87c3127e805a624b755
timsa <31769013+541263@users.noreply.github.com>
2023-08-11 12:35:45 +0500

    Update application.lua
    previous version havn't worked 
    I replaced init section and pin number, will try

--- application.lua
+++ application.lua
@@ -1,12 +1,14 @@
-
-SDAPIN = 4
-SCLPIN = 5
-TSL_ADDR = tsl2561.ADDRESS_FLOAT -- tsl2561.ADDRESS_GND || tsl2561.ADDRESS_VDD depends on witch gap soldered
+local tsl2561 = require "tsl2561" -- ???
+local i2cID = 0 -- ???
+local SDAPIN = 2 -- pin 2 = D2 = GPIO4
+local SCLPIN = 1 -- pin 1 = D1 = GPIO5
+local TSL_ADDR = tsl2561.ADDRESS_FLOAT -- tsl2561.ADDRESS_GND || tsl2561.ADDRESS_VDD depends on witch gap soldered
+i2c.setup(i2cID, SDAPIN, SCLPIN, i2c.SLOW) -- i don't know if this line is needed
+tsl2561.init(SDAPIN, SCLPIN, TSL_ADDR, tsl2561.PACKAGE_T_FN_CL)
 
 function getlux()
   toilet = nil
 
-  -- toilet
   status = tsl2561.init(SDAPIN, SCLPIN, TSL_ADDR, tsl2561.PACKAGE_T_FN_CL)
   if status == tsl2561.TSL2561_OK then
     toilet = tsl2561.getlux()
