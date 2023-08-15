6acd6f4a99675c162b575c7b62b7ac627d4fbc82
timsa <31769013+541263@users.noreply.github.com>
2023-08-15 09:52:24 +0500

    Update application2.lua

--- application2.lua
+++ application2.lua
@@ -24,29 +24,47 @@ function readbyte(bus, device, reg)
     return string.byte(b)
 end
 
+function sendlux(lux1, lux2)
+  http.get("http://host/lights.php?lux1="..lux1.."&lux2="..lux2, nil, function(code, data)
+      if (code < 0) then
+      elseif (code == 200) then
+      else
+        print(code, data) -- just for debug
+      end
+    end)
+end
+
+function loop()
+    -- light1
+    writebyte(I2C_ID, TSL1_ADDR, 0x80, 0x03) -- enable
+    writebyte(I2C_ID, TSL1_ADDR, 0x81, 0x02) -- set timing 402ms & gain 1x
+    writebyte(I2C_ID, TSL1_ADDR, 0x80, 0x00) -- disable
+    tmr.delay(1000)
+    writebyte(I2C_ID, TSL1_ADDR, 0x80, 0x03) -- enable
+    tmr.delay(500000)
+    c = readbyte(I2C_ID, TSL1_ADDR, 0x8C) -- read 0 channel (fullspectrum low)
+    d = readbyte(I2C_ID, TSL1_ADDR, 0x8D) -- read 0 channel (fullspectrum high)
+    l1=c+d*256
+    
+    -- light2
+    writebyte(I2C_ID, TSL2_ADDR, 0x80, 0x03) -- enable
+    writebyte(I2C_ID, TSL2_ADDR, 0x81, 0x02) -- set timing 402ms & gain 1x
+    writebyte(I2C_ID, TSL2_ADDR, 0x80, 0x00) -- disable
+    tmr.delay(1000)
+    writebyte(I2C_ID, TSL2_ADDR, 0x80, 0x03) -- enable
+    tmr.delay(500000)
+    c = readbyte(I2C_ID, TSL2_ADDR, 0x8C) -- read 0 channel (fullspectrum low)
+    d = readbyte(I2C_ID, TSL2_ADDR, 0x8D) -- read 0 channel (fullspectrum high)
+    l2=c+d*256
+    sendlux(l1,l2)
+    -- print("L1="..l1.." L2="..l2)
+end
+
+print("start i2c")
 i2c.setup(I2C_ID, SDAPIN, SCLPIN, i2c.FAST)
 
--- light1
-writebyte(I2C_ID, TSL1_ADDR, 0x80, 0x03) -- enable
-writebyte(I2C_ID, TSL1_ADDR, 0x81, 0x02) -- set timing 402ms & gain 1x
-writebyte(I2C_ID, TSL1_ADDR, 0x80, 0x00) -- disable
-tmr.delay(1000)
-writebyte(I2C_ID, TSL1_ADDR, 0x80, 0x03) -- enable
-tmr.delay(500000)
-c = readbyte(I2C_ID, TSL1_ADDR, 0x8C) -- read 0 channel (fullspectrum low)
-d = readbyte(I2C_ID, TSL1_ADDR, 0x8D) -- read 0 channel (fullspectrum high)
-lux1=c+d*256
+print("set dns")
+net.dns.setdnsserver("8.8.8.8",1)
 
--- light2
-writebyte(I2C_ID, TSL2_ADDR, 0x80, 0x03) -- enable
-writebyte(I2C_ID, TSL2_ADDR, 0x81, 0x02) -- set timing 402ms & gain 1x
-writebyte(I2C_ID, TSL2_ADDR, 0x80, 0x00) -- disable
-tmr.delay(1000)
-writebyte(I2C_ID, TSL2_ADDR, 0x80, 0x03) -- enable
-tmr.delay(500000)
-c = readbyte(I2C_ID, TSL2_ADDR, 0x8C) -- read 0 channel (fullspectrum low)
-d = readbyte(I2C_ID, TSL2_ADDR, 0x8D) -- read 0 channel (fullspectrum high)
-lux2=c+d*256
-
-print(lux1)
-print(lux2)
+print("start infinite loop ")
+tmr.create():alarm(10000, tmr.ALARM_AUTO, loop)
